66672f522866c8984909601ea72a9786a31e0140
AspieSoft <aspiesoftweb@gmail.com>
2022-02-03 15:05:30 -0500

    Added css for Progressive WordPress (PWA)

--- css-modifications-for-neve.php
+++ css-modifications-for-neve.php
@@ -5,7 +5,7 @@
 /*
 Plugin Name: Css Modifications For Neve (Beta)
 Description: Css and JS modifications for The Neve Wordpress Theme.
-Version: 0.4.0
+Version: 0.4.1
 Author: AspieSoft
 Author URI: https://www.aspiesoft.com
 License: GPLv2 or later
@@ -37,7 +37,7 @@ if(!class_exists('CssModificationsForNeve')){
 
   class CssModificationsForNeve{
 
-    private $ver = '0.3.7';
+    private $ver = '0.4.1';
 
     function register(){
       add_action('wp_enqueue_scripts', array($this, 'enqueue'));
